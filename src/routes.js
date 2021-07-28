e933e31e85cf7f57083fed63231e95e86647b066
dev-rafael92as <77762757+dev-rafael92as@users.noreply.github.com>
2021-07-28 00:35:21 -0300

    add template engine - ejs

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const routes = express.Router();
 
-// request / response
-routes.get('/', (request, response) => {
-  return response.sendFile(__dirname + "/views/index.html")
-})
+const views = __dirname + "/views/"
 
-routes.get('/index.html', (req, res) => {
-  return res.redirect('/')
-})
+// req / res
+routes.get('/', (req, res) => res.render(views + "index"))
+routes.get('/job', (req, res) => res.render(views + "job"))
+routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
+routes.get('/profile', (req, res) => res.render(views + "profile"))
+
 module.exports = routes
