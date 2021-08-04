418374b195dee85d094ab629a7c0bdc54814a0b3
dev-rafael92as <77762757+dev-rafael92as@users.noreply.github.com>
2021-08-04 02:03:25 -0300

    add + routes / add inicial func. adc proj.

--- src/routes.js
+++ src/routes.js
@@ -13,10 +13,32 @@ const profile = {
   "vacation-per-year": 4
 }
 
+const jobs = [
+  {
+  id: 1,
+    name: 'Pizzaria Guloso',
+    "daily-hours": 2,
+    "total-hours": 60,
+    created_at: Date.now()
+  }
+]
 
 // req / res
-routes.get('/', (req, res) => res.render(views + "index"))
+routes.get('/', (req, res) => res.render(views + "index", { jobs }))
 routes.get('/job', (req, res) => res.render(views + "job"))
+routes.post('/job', (req, res) => {
+  // req.body = { name: 'rafa', 'daily-hours': '8', 'total-hours': '1' }
+  const lastId = jobs[jobs.length - 1]?.id || 1
+
+  jobs.push({
+    id: lastId + 1,
+    name: req.body.name,
+    "daily-hours": req.body["daily-hours"],
+    "total-hours": req.body["total-hours"],
+    created_at: Date.now() // atribuindo uma nova data
+  })
+  return res.redirect('/')
+})
 routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
 routes.get('/profile', (req, res) => res.render(views + "profile", { profile }))
 
--- src/server.js
+++ src/server.js
@@ -8,6 +8,11 @@ server.set('view engine', 'ejs')
 //habilitar arquivos statics (estáticos)
 server.use(express.static("public"))
 
+// usar o req.body (liberação)
+server.use(express.urlencoded( {extended: true} ))
+
 // routes
 server.use(routes)
 server.listen(3000, () => console.log("server rodando"))
+
+// server.use() = Pode ser usado para setar/iniciar/chavear configurações no servidor 
